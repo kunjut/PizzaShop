55fe783ac91e4a6f813ec76ae771a2122094c6a3
Vadim Dimnich <v.dimnich@gmail.com>
2019-09-10 16:41:24 +0300

    check orders func

--- public/app.js
+++ public/app.js
@@ -37,3 +37,15 @@ function check_basket() {
     //alert(total);
     return total;
 }
+
+function check_orders() {
+    var orders = '';
+    for (var i = 0; i < window.localStorage.length; i++) {
+        var key = window.localStorage.key(i);
+        var z = window.localStorage.getItem(key);
+        if(key.indexOf('product_') == 0) {
+            orders = orders + key + '=' + z + ',';  
+        }
+    }
+    return orders;
+}
