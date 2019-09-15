20f0e00d2cb5f673cca2b86f7fac122effe60660
Vadim Dimnich <v.dimnich@gmail.com>
2019-09-15 17:25:14 +0300

    HW_p1: model Order, connect with DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_04_120447) do
+ActiveRecord::Schema.define(version: 2019_09_15_135513) do
+
+  create_table "orders", force: :cascade do |t|
+    t.text "name"
+    t.text "phone"
+    t.text "adress"
+    t.text "orders_input"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
